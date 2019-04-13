7bc5736574354a838bbd801ce7db1cde94eddc8a
Levi Murray <levidavidmurray@gmail.com>
2019-04-12 21:16:31 -0700

    Created router navigation between categories to dynamically pull specific category items

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Category from '../views/Category';
 
@@ -8,19 +8,43 @@ import './styles/App.scss';
 const pathCategories = ['bakery', 'deli', 'meat', 'dairy', 'pasta', 'produce'];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      navbar: this.createCategoryNavBar(),
+      categoryPaths: this.createCategoryPaths()
+    }
+  }
+  
+  createCategoryNavBar() {
+    return pathCategories.map(category => {
+      let path = `/${category}`;
+      return (
+        <div key={category} className="link">
+          <Link to={path}>{ category }</Link>
+        </div>
+      );
+    });
+  }
 
   createCategoryPaths() {
     return pathCategories.map(category => {
       let path = `/${category}`;
-      return <Route path={path} render={() => <Category name={category} />} />
+      return <Route key={category} path={path} render={() => <Category name={category} />} />
     });
   }
 
   render() {
     return (
-      <Router>
-        { this.createCategoryPaths() }
-      </Router>
+      <div>
+        <Router>
+          <div className="navbar">
+            { this.state.navbar }
+          </div>
+          { this.state.categoryPaths }
+        </Router>
+      </div>
     );
   }
 }
---
+++ client/src/views/CategoryDisplay.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class CategoryDisplay extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
