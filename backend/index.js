298e97fb8748d43b9d351054083f5869fcde894d
Levi Murray <levidavidmurray@gmail.com>
2019-04-19 18:45:59 -0700

    Converted spaces to tabs. Added yarn.lock

--- backend/index.js
+++ backend/index.js
@@ -7,22 +7,22 @@ const app = express();
 app.use(cors());
 
 const categoryPathMap = {
-  bakery: 'bakery',
-  dairy: 'dairy',
-  deli: 'deli',
-  meat: 'meat_and_seafood',
-  pasta: 'pasta_sauces_grains',
-  produce: 'produce'
+	bakery: 'bakery',
+	dairy: 'dairy',
+	deli: 'deli',
+	meat: 'meat_and_seafood',
+	pasta: 'pasta_sauces_grains',
+	produce: 'produce'
 };
 
 Object.entries(categoryPathMap).forEach(categoryPath => {
-  app.get(`/${categoryPath[0]}`, (req, res) => {
-    GroceryItems(categoryPath[1]).findAll({ limit: 20 }).then(items => {
-      res.send(items);
-    });
-  });
+	app.get(`/${categoryPath[0]}`, (req, res) => {
+		GroceryItems(categoryPath[1]).findAll({limit: 20}).then(items => {
+			res.send(items);
+		});
+	});
 });
 
 app.listen(4070, () => {
-  console.log('Listening on port 3000');
+	console.log('Listening on port 3000');
 });
