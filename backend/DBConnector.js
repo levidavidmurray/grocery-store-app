7072c3a7b8af0f86b66c4ccde9fb238205ffac8a
Levi Murray <levidavidmurray@gmail.com>
2019-04-19 19:02:04 -0700

    Migrating from react to vue

--- backend/DBConnector.js
+++ backend/DBConnector.js
@@ -4,6 +4,7 @@ const Sequelize = require("sequelize");
 const database = process.env.DB_NAME;
 const username = process.env.DB_USER;
 const password = process.env.DB_PASS;
+
 const connector = {host: process.env.DB_HOST, dialect: "mariadb"};
 
 const sequelize = new Sequelize(database, username, password, connector);
