befb44023a5d6b1933de3f76790e9e992c7ce86e
Levi Murray <levidavidmurray@gmail.com>
2019-04-21 19:37:39 -0700

    Pulling redux store cart data into shopping cart view

--- client/src/views/Cart.vue
+++ client/src/views/Cart.vue
@@ -1,10 +1,29 @@
 <template>
-	<div>Shopping Cart</div>
+	<div>
+		<h1>Shopping Cart</h1>
+		<div class="cart-item" v-for="item in cartItems" :key="item.sku">
+			<div>{{ item.name }} - {{ item.currentPrice }}</div>
+		</div>
+	</div>
 </template>
 
 <script>
 	export default {
-		name: "Cart"
+		name: "Cart",
+
+		data() {
+			return {
+				cartItems: []
+			}
+		},
+
+		created() {
+			this.cartItems = this.$store.getters.cartItemsAll;
+		},
+
+		mounted() {
+			console.log(this.cartItems);
+		}
 	}
 </script>
 
